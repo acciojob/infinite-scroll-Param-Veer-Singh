b80dc0f2d988cac2eb7242bd7adb243669fb6102
Param-Veer-Singh <122340319+Param-Veer-Singh@users.noreply.github.com>
2023-10-30 20:08:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here!
+const infiList = document.getElementById("infi-list");
+
+let j = 11;
+function addTwoElements(){
+    for(let i=1; i<=2; i++){
+        const li = document.createElement("li");
+        li.innerText= `Item ${j++}`;
+        infiList.appendChild(li);
+    }
+}
+for(let i=1; i<=10; i++){
+    const li = document.createElement("li");
+    li.innerText= `Item ${i}`;
+    infiList.appendChild(li);
+}
+
+infiList.addEventListener("scroll",()=>{
+
+    if(infiList.scrollHeight - infiList.clientHeight - infiList.scrollTop <= 4){
+        addTwoElements();
+    }
+})
 
